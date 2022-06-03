e2db4b81825e0e47ea0ff5e1359f6152beadf8f4
aryan katiyar <69989774+inumaki@users.noreply.github.com>
2022-06-04 02:00:50 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,97 @@
+let button = document.querySelector('.btn');
+let taskcontainer = document.querySelector('.tasklist');
+let bottom_button_b2 = document.querySelector('.b2');
+
+
+let inputlist = document.querySelector('.list');
+console.dir(inputlist)
+button.addEventListener('click',additems)
+bottom_button_b2.addEventListener('click',deleteselectedlist)
+
+function deleteselectedlist(e)
+{
+console.log(selectedListId)
+let list_element=   document.getElementById(selectedListId)
+console.log(list_element)
+taskcontainer.removeChild(list_element);
+save();
+render();
+
+}
+
+
+const LOCAL_STORAGE_LIST_KEY= "task.lists";
+const LOCAL_STORAGE_LIST_ID_KEY= "task.selectedListId"
+let lists= JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY))||[]
+let selectedListId= localStorage.getItem(LOCAL_STORAGE_LIST_ID_KEY)
+render()
+
+taskcontainer.addEventListener('click',e=>{
+
+  if(e.target.tagName.toLowerCase()==='li')
+  {  selectedListId= e.target.dataset.listID;
+    
+  }
+}
+)
+
+
+function render()
+{
+  clearPrevious()
+  for(let list of lists)
+  {
+  let li  = document.createElement('li')
+  
+  li.dataset.listID= list.id;
+    li.id= list.id;
+  if(list.id==selectedListId)
+    li.classList.add('selected')
+  li.classList.add('preactive')
+ 
+  li.innerText= list.name;
+  taskcontainer.append(li);
+  }
+
+}
+function clearPrevious()
+{
+  while(taskcontainer.firstChild)
+  {
+    taskcontainer.removeChild(taskcontainer.firstChild);
+  }
+
+}
+function save()
+{
+  localStorage.setItem(LOCAL_STORAGE_LIST_KEY,JSON.stringify(lists));
+  localStorage.setItem(LOCAL_STORAGE_LIST_ID_KEY,JSON.stringify(selectedListId));
+}
+
+
+
+
+
+function additems(e)
+{
+e.preventDefault()
+if(inputlist.value==null || inputlist.value=='')
+return ;
+if(e.target.id=='addlist')
+{
+let li  = document.createElement('li')
+li.classList.add('preactive')
+li.innerText= inputlist.value;
+let cl = createList(inputlist.value)
+inputlist.value='';
+lists.push(cl);
+render();
+save();
+}
+
+
+}
+function createList(lname)
+{
+  return {id:Date.now().toString(),name:lname,tasks:[]}
+}
